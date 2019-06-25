124e41e2d748e38869c217dc953180fb0e727f13
Karan Kumar <karu19961j@gmail.com>
2019-06-25 11:34:00 +0530

    camera-s3-upload

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  constructor(private camera: Camera, private sanitized: DomSanitizer) {}
+  image: SafeResourceUrl = '';
+  openCam() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
 
-  constructor() {}
+    this.camera.getPicture(options).then(
+      imageData => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64 (DATA_URL):
+        //alert(imageData)
 
+        this.image = (window as any).Ionic.WebView.convertFileSrc(imageData);
+      },
+      err => {
+        // Handle error
+        alert('error ' + JSON.stringify(err));
+      }
+    );
+  }
 }
